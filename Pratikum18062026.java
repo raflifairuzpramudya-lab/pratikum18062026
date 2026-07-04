388e33096b41a13ed2731300a73bc547b6a6d00a
raflifairuzpramudya-lab <raflifairuzpramudya@gmail.com>
2026-07-05 06:25:03 +0700

    Add files via upload

---
+++ Pratikum18062026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pratikum18062026;
+
+/**
+ *
+ * @author asus
+ */
+public class Pratikum18062026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
